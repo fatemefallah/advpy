62a5338b73ce88ce05dd10861180ddd9d3b9c454
fatemefa <fatemefallah99@gmail.com>
2022-05-24 16:48:33 +0430

    karim_names new added

---
+++ karim_names.py
@@ -0,0 +1,11 @@
+n = int(input())
+ans = 0
+for i in range(n):
+    s = input()
+    t = ''
+    for ch in s:
+        if ch not in t:
+            t += ch 
+    if ans < len(t): # ans = max(ans, len(t))
+        ans = len(t)
+print(ans)
