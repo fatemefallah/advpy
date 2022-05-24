20b73e901d897a9f9bbf5f2b649a8981105d6c37
fatemefa <fatemefallah99@gmail.com>
2022-05-24 18:06:03 +0430

    how day is he added

---
+++ how_day_is_he.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+def day_calculator(date):
+    date = datetime.strptime(date, '%Y-%m-%d')
+    sajjad_bdate = datetime.strptime("1999-01-14",'%Y-%m-%d')
+    flag = (date - sajjad_bdate).days
+    if flag < 0:
+        res = "Not yet born"
+    else:
+        res = flag
+    return res
