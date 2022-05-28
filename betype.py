60c2d497565b19652da4ad4ca0c7f4fea2cf4887
fatemefa <fatemefallah99@gmail.com>
2022-05-28 13:37:26 +0430

    betype added

---
+++ betype.py
@@ -0,0 +1,10 @@
+inp = input()
+inp = inp.strip('=')
+res = []
+for i in inp:
+    if i != '=' and len(inp) != 0:
+        res.append(i)
+    elif i == '=':
+        res.pop()
+
+print(''.join(res))
