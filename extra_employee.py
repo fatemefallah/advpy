c53f272e55754f40aa5dbe64c27a079d8cd79290
fatemefa <fatemefallah99@gmail.com>
2022-05-30 20:24:47 +0430

    most_freq added

---
+++ extra_employee.py
@@ -0,0 +1,35 @@
+n = int(input())
+names = []
+for i in range(n):
+    f, l = input().split()
+    names.append(f)
+
+
+def most_freq(List):
+    dic = {}
+    count, it = 0, ''
+    for item in List:
+        dic[item] = dic.get(item, 0) + 1
+        if dic[item] >= count:
+            count = dic[item]
+
+    if all(value == 1 for value in dic.values()):
+        return 1
+    return count
+
+
+print(most_freq(names))
+
+
+"""n = int(input())
+dic = {}
+col = 1
+for i in range(n):
+    f, l = input().split()
+    if f in dic:
+        col += 1
+    else:
+        dic[f] = l
+print(col)"""
+
+
