f9b9ea3c77b45666c69fe706a0f6e685edf19124
fatemefa <fatemefallah99@gmail.com>
2022-06-05 21:45:22 +0430

    pycode added

--- pyCode.py
+++ pyCode.py
@@ -1,4 +1,15 @@
+import re
+
+
 def solve(path):
+    cnt = 0
+    with open(path, 'r') as file:
+        for row in file:
+            line = str(row).strip()
+            if not re.match(r'^#|^$', line):
+                cnt += 1
+    return cnt
 
 
-
+path1 = 'karim_names.py'
+print(solve(path1))
