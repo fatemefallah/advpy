f5f00692cf95767c1514fe45febb266f24338b78
fatemefa <fatemefallah99@gmail.com>
2022-06-17 13:41:31 +0430

    some guiding comment added + quera code for explorer

--- explore.py
+++ explore.py
@@ -12,7 +12,8 @@ def explore(ttype, address):
     for i in range(len(files)):
         for j in range(len(files[i])):
             files[i][j] = files[i][j].lower()
-            match = re.search(f'\.{ttype}$', files[i][j])
+            match = re.search(f'\.{ttype}$', files[i][j])   # in this line, the entire title and ext will convert to
+            # lowercase. Quera did it better. at the end of my code
             if match:
                 cnt += 1
                 dic[adds[i]] = cnt
@@ -21,3 +22,18 @@ def explore(ttype, address):
 
 
 print(explore("pY", ".\\CSV"))
+
+"""
+import os
+ 
+def explore(extension, addr):
+    result = dict()
+    for obj in os.walk(addr):
+        for name in obj[2]:
+            if "." in name and name.split('.')[-1].lower() == extension.lower():
+                try:
+                    result[obj[0]] += 1
+                except KeyError:
+                    result[obj[0]] = 1
+    return result
+"""
