2884547cbdba4ee9ab6794e4e87ccff636614383
fatemefa <fatemefallah99@gmail.com>
2022-06-22 20:35:26 +0430

    words check added

---
+++ periodic_prc/words_check.py
@@ -0,0 +1,26 @@
+def words_check(text):
+    res = {}
+    text = text.split('\n')
+    for item in text:
+        item = item.split(' ')
+        for i, word in enumerate(item):
+            length = len(word) / 2
+            cnt = 0
+            for ch in word:
+                if ch.isalpha() is False:
+                    cnt += 1
+                    word = word.replace(ch, '')
+            if 0 <= cnt < length:
+                word = word.title()
+                if word in res:
+                    res[word] += 1
+                else:
+                    res[word] = 1
+    return res
+
+
+# Local Test
+print(words_check("""HeLLLO_O My________________FRIEND
+HOW ARE YOUUUUU?___?
+I Don'T KNow Y_O_U_R_N_A_M_E yet !!!!!!!!"""))
+# {'Hellloo': 1, 'How': 1, 'Are': 1, 'Youuuuu': 1, 'I': 1, 'Dont': 1, 'Know': 1, 'Yourname': 1, 'Yet': 1}
