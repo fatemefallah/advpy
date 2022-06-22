ff7169dc71eae71f43033b3815c41edc7b5adb61
fatemefa <fatemefallah99@gmail.com>
2022-06-22 20:43:47 +0430

    quera code added to words_check

--- periodic_prc/words_check.py
+++ periodic_prc/words_check.py
@@ -24,3 +24,19 @@ print(words_check("""HeLLLO_O My________________FRIEND
 HOW ARE YOUUUUU?___?
 I Don'T KNow Y_O_U_R_N_A_M_E yet !!!!!!!!"""))
 # {'Hellloo': 1, 'How': 1, 'Are': 1, 'Youuuuu': 1, 'I': 1, 'Dont': 1, 'Know': 1, 'Yourname': 1, 'Yet': 1}
+
+"""
+from collections import defaultdict
+
+def check_good_word(word):
+    good_word = list(filter(lambda letter: letter.isalpha(), word))
+    return "".join(good_word).capitalize() if len(good_word) > len(word) / 2 else ""
+
+def words_check(text):
+    result = defaultdict(int)
+    for word in text.split():
+        if good_word:=check_good_word(word):
+            result[good_word] += 1
+    return dict(sorted(result.items(), key=lambda item: item[0]))
+
+"""
