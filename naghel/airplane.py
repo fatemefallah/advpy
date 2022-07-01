69b4186ec267dca258a9d5928e9e170000ac5f9c
FatemeFa <fatemefallah99@gmail.com>
2022-07-02 03:35:33 +0430

    djnvffv

--- naghel/airplane.py
+++ naghel/airplane.py
@@ -8,10 +8,22 @@ class Airplane(FlyingVehicle, GroundVehicle):
         self.number_of_crew = number_of_crew
         self.captain = captain
         super().__init__(**kwargs)
+        
+    ''''def outp(self):
+        print(self.name,self.airline, self.number_of_wheels, self.captain, self.price, self.fuel, self.number_of_fins, self.max_speed)'''
 
 
 class B707(Airplane):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        
+    '''def outp(self):
+        print(self.name, self.airline, self.captain, self.number_of_wheels)'''
 
 
+'''v = Airplane(name="Airplane", price=500000, number_of_seats=300, max_speed=400, number_of_wheels=10,
+    steering_wheel="Manual", fuel="Oil", number_of_fins=2, airline="Quera Air", number_of_crew=65,
+    captain="Bagher")
+v = B707(name="SAliB", price=850000, number_of_seats=850, max_speed=500, number_of_wheels=18,
+                 steering_wheel="Manual", fuel="Oil", number_of_fins=2, airline="Quera Air", number_of_crew=65,
+                 captain="Bagher")'''
--- naghel/vehicle.py
+++ naghel/vehicle.py
@@ -4,4 +4,9 @@ class Vehicle:
         self.price = price
         self.number_of_seats = number_of_seats
         self.max_speed = max_speed
+        
+'''    def outp(self):
+        print(self.name, self.number_of_seats, self.price, self.max_speed)
 
+v = Vehicle(name="Car", price=5000, number_of_seats=5, max_speed=280)
+v.outp()'''
