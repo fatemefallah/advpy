5b2f0a25e253d39034e72e6238f30da8bb5b0348
FatemeFa <fatemefallah99@gmail.com>
2022-07-23 22:24:17 +0430

    strint added.

--- strint.py
+++ strint.py
@@ -1,3 +1,5 @@
+import re
+
 class Strint(int):
     
     def __lt__(self, other):
@@ -22,12 +24,21 @@ class Strint(int):
         return int(str(self) + str(other))
 
     def __sub__(self, other):
-        return int(str(self) - str(other))
-        '''if type(res) not int:
-            raise 'The subtraction is not valid!''''
+        sub_res = int(re.sub(str(other),"", str(self)))
+        if sub_res < 0:
+            raise 'The subtraction is not valid'
+        else:
+            return sub_res
 
     def __len__(self):
         return len(str(self))
 
     def __call__(self):
         pass
+
+
+a = Strint(8439)
+b = Strint(39)
+d = b - a
+print(d)
+print(type(d))
